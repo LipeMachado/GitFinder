6a6d7520d19cd2fa02a48f9c561f60cd032e36b7
LipeMachado <silvalipe765@gmail.com>
2023-10-17 10:12:59 -0300

    Fixed Responsive

--- src/Components/ProfileCard.tsx
+++ src/Components/ProfileCard.tsx
@@ -8,11 +8,11 @@ export function ProfileCard({ avatar_url, followers, following, location, name }
             <div className="w-[90%] sm:w-[500px] h-full sm:h-[300px] mt-6 sm:mt-12 rounded border-[1px] border-[#7858A6] bg-[#19191F] text-[#f6f6f7] select-none">
                 <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 items-center py-4 px-5">
                     <div className="">
-                        <img className="w-[70px] h-[60px] rounded" src={avatar_url} />
+                        <img className="w-[70px] h-[70px] sm:h-[60px] rounded" src={avatar_url} />
                     </div>
                     <div className="w-full flex flex-col sm:flex-row gap-2 sm:gap-0 items-center justify-around">
-                        <div className="flex flex-col gap-1 justify-center mr-0 sm:mr-10">
-                            <h2 className="text-sm sm:text-lg font-bold">{name}</h2>
+                        <div className="flex flex-col gap-1 justify-center mx-0 sm:mx-3">
+                            <h2 className="text-sm sm:text-base font-bold text-center">{name}</h2>
                             <div className="flex justify-center sm:justify-start items-center gap-1">
                                 <BiMap />
                                 <span className="sm:text-xs">{location}</span>
