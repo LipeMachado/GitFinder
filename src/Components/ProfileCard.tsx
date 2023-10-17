6c1b5227423a2db9961f2c4e11179181fb5dbe5e
LipeMachado <silvalipe765@gmail.com>
2023-10-17 10:04:03 -0300

    Fixed Style

--- src/Components/ProfileCard.tsx
+++ src/Components/ProfileCard.tsx
@@ -11,9 +11,9 @@ export function ProfileCard({ avatar_url, followers, following, location, name }
                         <img className="w-[70px] h-[60px] rounded" src={avatar_url} />
                     </div>
                     <div className="w-full flex flex-col sm:flex-row gap-2 sm:gap-0 items-center justify-around">
-                        <div className="flex flex-col gap-1 items-center justify-center text-center">
+                        <div className="flex flex-col gap-1 justify-center mr-0 sm:mr-10">
                             <h2 className="text-sm sm:text-lg font-bold">{name}</h2>
-                            <div className="flex justify-center items-center gap-1 mr-0 sm:mr-10">
+                            <div className="flex justify-center sm:justify-start items-center gap-1">
                                 <BiMap />
                                 <span className="sm:text-xs">{location}</span>
                             </div>
