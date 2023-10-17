f73b4fe5fe8f364c17f4cdf95fe3c098be7327e5
LipeMachado <silvalipe765@gmail.com>
2023-10-16 22:51:58 -0300

    Add Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FiGithub } from 'react-icons/fi';
 import { SearchBar } from './Components/SearchBar';
+import { ProfileCard } from './Components/ProfileCard';
 
 export function App() {
   return (
@@ -8,11 +9,14 @@ export function App() {
         <nav className="w-full pt-6 flex items-center justify-center">
           <FiGithub size="40" color="#F6F6F7" />
         </nav>
-        <div className="w-full flex items-center justify-center mb-10">
-          <h1 className="text-4xl text-[#f6f6f7] pt-10">Seja bem vindo(a) ao GitFinder</h1>
+        <div className="w-full flex items-center justify-center text-center mb-10">
+          <h1 className="text-3xl md:text-4xl text-[#f6f6f7] pt-10">Seja bem vindo(a) ao GitFinder</h1>
         </div>
-        <div>
+        <div className="w-full flex justify-center items-center">
           <SearchBar />
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <ProfileCard />
         </div>
       </div>
     </>
--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -3,11 +3,9 @@ import { BiSearch } from "react-icons/bi";
 export function SearchBar() {
     return (
         <>
-            <div className="w-full flex justify-center items-center">
-                <div className="w-[500px] relative flex justify-center items-center">
-                    <BiSearch className="absolute left-2" size={30} color="#F6F6F7" />
-                    <input className="w-full py-2 pl-12 pr-3 rounded border-[1px] border-[#7858A6] bg-[#19191F] placeholder:-[#7C7C8A] text-[#f6f6f7] " placeholder="Aqui você encontrará todas as pessoas do github." />
-                </div>
+            <div className="w-[90%] sm:w-[500px] relative flex justify-center items-center">
+                <BiSearch className="absolute left-2" size={30} color="#F6F6F7" />
+                <input className="w-full py-2 pl-12 pr-3 rounded border-[1px] border-[#7858A6] bg-[#19191F] placeholder:-[#7C7C8A] text-[#f6f6f7] " placeholder="Pesquise um usuário do github." />
             </div>
         </>
     )
