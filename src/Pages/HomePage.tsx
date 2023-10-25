b9933308f928546465c80ed5dffa6664d19c0bc0
LipeMachado <silvalipe765@gmail.com>
2023-10-25 11:38:24 -0300

    Remove div

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -39,21 +39,19 @@ export function HomePage() {
 
     return (
         <>
-            <div>
-                <nav className="w-full pt-6 flex items-center justify-center">
-                    <FiGithub size="40" color="#F6F6F7" />
-                </nav>
-                <div className="w-full flex flex-col items-center justify-center text-center mb-10 select-none">
-                    <h1 className="text-3xl md:text-4xl text-[#f6f6f7] pt-10">Seja bem vindo(a) ao GitFinder</h1>
-                    <span className="text-lg md:text-xl text-gray-400 pt-3">Clique na foto para te redirecionar ao GitHub.</span>
-                </div>
-                <div className="w-full flex justify-center items-center">
-                    <SearchBar LoadUser={loadUser} />
-                </div>
-                <div className="w-full flex justify-center items-center">
-                    {user && <ProfileCard {...user} />}
-                    {error && <Error />}
-                </div>
+            <nav className="w-full pt-6 flex items-center justify-center">
+                <FiGithub size="40" color="#F6F6F7" />
+            </nav>
+            <div className="w-full flex flex-col items-center justify-center text-center mb-10 select-none">
+                <h1 className="text-3xl md:text-4xl text-[#f6f6f7] pt-10">Seja bem vindo(a) ao GitFinder</h1>
+                <span className="text-lg md:text-xl text-gray-400 pt-3">Clique na foto para te redirecionar ao GitHub.</span>
+            </div>
+            <div className="w-full flex justify-center items-center">
+                <SearchBar LoadUser={loadUser} />
+            </div>
+            <div className="w-full flex justify-center items-center">
+                {user && <ProfileCard {...user} />}
+                {error && <Error />}
             </div>
         </>
     )
