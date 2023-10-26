e03b924f5c73017772c5b648612dac8a0ef8548d
LipeMachado <silvalipe765@gmail.com>
2023-10-26 01:42:59 -0300

    Change Style

--- src/Pages/RepoPage.tsx
+++ src/Pages/RepoPage.tsx
@@ -26,11 +26,12 @@ export function RepoPage() {
         <>
             <section className="relative z-10">
                 <div className="text-white">
-                    <div className="w-full flex flex-col items-center justify-center gap-10 text-center mb-10 select-none">
+                    <div className="w-full flex flex-col items-center justify-center gap-8 text-center mb-10 select-none">
                         <h1 className="text-3xl md:text-4xl text-[#f6f6f7] pt-10">Repositórios</h1>
+                        <span className="text-lg md:text-xl text-gray-400 pt-3">Clique no link do repositório para se redirecionar ao GitHub.</span>
                         <Link to="/" className="flex justify-center items-center gap-3 py-2 px-3 group">
-                            <FiArrowLeft size={18} className="group-hover:-translate-x-1 transform transition-transform" />
-                            <span className="group-hover:text-[#e3e3e3] transition-colors">Voltar</span>
+                            <FiArrowLeft size={20} className="group-hover:text-[#b383f6] group-hover:-translate-x-1 transform transition-transform" />
+                            <span className="group-hover:text-[#b383f6] transition-colors text-xl">Voltar</span>
                         </Link>
                     </div>
                     <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] justify-center gap-5 px-10 mt-[60px]">
