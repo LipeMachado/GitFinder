566cc72bec412ea468e58247237c3d66dce3f0ca
LipeMachado <silvalipe765@gmail.com>
2023-10-26 01:36:54 -0300

    Add Style Repositories

--- src/Components/RepoCard.tsx
+++ src/Components/RepoCard.tsx
@@ -4,9 +4,9 @@ import { RepositoresProps } from "../Types/repos";
 export function RepoCard({ name, html_url }: RepositoresProps) {
     return (
         <>
-            <div className="w-full flex flex-col justify-center items-center gap-3 py-3 px-3 sm:mt-12 rounded border-[1px] border-[#7858A6] bg-[#19191F] text-[#f6f6f7] text-center">
-                <h1 className="">{name}</h1>
-                <Link to={html_url} target="_blank">Link do Repositório</Link>
+            <div className="w-full flex flex-col justify-center items-center gap-3 py-3 px-4 sm:mt-12 rounded border-[1px] border-[#7858A6] bg-[#19191F] text-[#f6f6f7] text-center">
+                <h1 className="font-bold text-lg max-w-full break-words">{name}</h1>
+                <Link to={html_url} target="_blank" className="text-[#8964bc] hover:text-[#b383f6] transition-colors">Link do Repositório</Link>
             </div>
         </>
     )
